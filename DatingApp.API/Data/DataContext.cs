938221a644e7a1132f7dccfdc7ac10e283c2bb66
Abhijeet <abhijeet.dhoke@gmail.com>
2020-01-23 09:28:53 +0530

    Auth and User Creation Changes

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -7,6 +7,8 @@ namespace DatingApp.API.Data
        public DataContext(DbContextOptions<DataContext> options):base(options){}
         
         public DbSet<value> Values{get ;set ;}
+        public DbSet<User> Users{get;set;}
+        
         
     }
 }
